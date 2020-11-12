2c00993abe95bb3723a4ff9727ff75eaec2b8eaa
wh8199 <819971938@qq.com>
2020-11-12 23:11:43 +0800

    Add some comment

--- config.go
+++ config.go
@@ -6,6 +6,16 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+/*
+	description: generate k8s config with token
+	params:
+		token: k8s config token
+		server: k8s server address
+		cacert: the cacert, if skipCA is equal true, this value will be ignored
+		username: the display username of k8s cluster
+		clustername: the display clustername of k8s clutser
+		skipCA: skip ca check or not, if this value is equal true, the cacert is ignored
+*/
 func GenerateTokenConfig(token, server, cacert, username, clustername string, skipCA bool) ([]byte, error) {
 	config := &K8SConfig{
 		APIVersion: "v1",
@@ -48,6 +58,17 @@ func GenerateTokenConfig(token, server, cacert, username, clustername string, sk
 	return data, nil
 }
 
+/*
+	description: generate k8s config with clientcert or key
+	params:
+		server: k8s server address
+		cacert: the cacert, if skipCA is equal true, this value will be ignored
+		clientcert: the clientcert of tls
+		clientkey: the clientkey of tls
+		username: the display username of k8s cluster
+		clustername: the display clustername of k8s clutser
+		skipCA: skip ca check or not, if this value is equal true, the cacert is ignored
+*/
 func GenerateCertConfig(server, cacert, clientcert, clientkey, username, clustername string, skipCA bool) ([]byte, error) {
 	config := &K8SConfig{
 		APIVersion: "v1",
@@ -91,6 +112,16 @@ func GenerateCertConfig(server, cacert, clientcert, clientkey, username, cluster
 	return data, nil
 }
 
+/*
+	description: generate k8s config with clientcert or key
+	params:
+		server: k8s server address
+		cacert: the cacert, if skipCA is equal true, this value will be ignored
+		username: the username of basicauth
+		password: the password of basicauth
+		clustername: the display clustername of k8s clutser
+		skipCA: skip ca check or not, if this value is equal true, the cacert is ignored
+*/
 func GenerateBasciAuthConfig(server, cacert, username, password, clustername string, skipCA bool) ([]byte, error) {
 	config := &K8SConfig{
 		APIVersion: "v1",
